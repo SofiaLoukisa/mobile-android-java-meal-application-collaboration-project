10cfa90d87446155d38225def84b75bce583a0a9
Sofia <sloukisa03@gmail.com>
2026-04-10 13:22:25 +0300

    implement api call in main activity and verify connection

--- app/src/main/java/com/example/mealapplication/MainActivity.java
+++ app/src/main/java/com/example/mealapplication/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.mealapplication;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +9,51 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.mealapplication.model.CategoryResponse;
+import com.example.mealapplication.remote.ApiClient;
+import com.example.mealapplication.remote.ApiService;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "API_TEST";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // API CALL
+        ApiService apiService = ApiClient.getClient().create(ApiService.class);
+
+        apiService.getCategories().enqueue(new Callback<CategoryResponse>() {
+            @Override
+            public void onResponse(Call<CategoryResponse> call, Response<CategoryResponse> response) {
+
+                if (response.isSuccessful() && response.body() != null) {
+
+                    int count = response.body().categories.size();
+                    Log.d(TAG, "Categories loaded: " + count);
+
+                } else {
+                    Log.d(TAG, "Response failed");
+                }
+            }
+
+            @Override
+            public void onFailure(Call<CategoryResponse> call, Throwable t) {
+                Log.e(TAG, "Error: " + t.getMessage());
+            }
         });
     }
 }
--- app/src/main/java/remote/ApiService.java
+++ app/src/main/java/remote/ApiService.java
@@ -1,4 +1,23 @@
-package remote;
+package com.example.mealapplication.remote;
+
+import com.example.testmealplanner.model.CategoryResponse;
+import com.example.testmealplanner.model.MealDetailResponse;
+import com.example.testmealplanner.model.MealResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 public interface ApiService {
+    @GET("categories.php")
+    Call<CategoryResponse> getCategories();
+
+    @GET("filter.php")
+    Call<MealResponse> getMealsByCategory(@Query("c") String category);
+
+    @GET("search.php")
+    Call<MealResponse> searchMealsByName(@Query("s") String query);
+
+    @GET("lookup.php")
+    Call<MealDetailResponse> getMealDetails(@Query("i") String mealId);
 }
