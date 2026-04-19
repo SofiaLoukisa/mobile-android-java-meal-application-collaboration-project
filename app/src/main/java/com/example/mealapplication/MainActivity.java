47bebf8665d52cc004824c8e8ae3df885cd7adc7
Sofia <sloukisa03@gmail.com>
2026-04-19 16:34:42 +0300

    add project architect

--- app/src/main/java/com/example/mealapplication/MainActivity.java
+++ app/src/main/java/com/example/mealapplication/MainActivity.java
@@ -1,59 +1,24 @@
-package com.example.mealapplication;
+package com.example.testmealplanner;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
-import com.example.mealapplication.model.CategoryResponse;
-import com.example.mealapplication.remote.ApiClient;
-import com.example.mealapplication.remote.ApiService;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
+import com.example.testmealplanner.ui.auth.LoginActivity;
+import com.example.testmealplanner.ui.browse.BrowseActivity;
+import com.example.testmealplanner.utils.UserPreferences;
 
 public class MainActivity extends AppCompatActivity {
-
-    private static final String TAG = "API_TEST";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-
-        // API CALL
-        ApiService apiService = ApiClient.getClient().create(ApiService.class);
-
-        apiService.getCategories().enqueue(new Callback<CategoryResponse>() {
-            @Override
-            public void onResponse(Call<CategoryResponse> call, Response<CategoryResponse> response) {
-
-                if (response.isSuccessful() && response.body() != null) {
-
-                    int count = response.body().categories.size();
-                    Log.d(TAG, "Categories loaded: " + count);
-
-                } else {
-                    Log.d(TAG, "Response failed");
-                }
-            }
-
-            @Override
-            public void onFailure(Call<CategoryResponse> call, Throwable t) {
-                Log.e(TAG, "Error: " + t.getMessage());
-            }
-        });
+        UserPreferences userPrefs = new UserPreferences(this);
+        Intent intent;
+        if (userPrefs.isLoggedIn()) {
+            intent = new Intent(this, BrowseActivity.class);
+        } else {
+            intent = new Intent(this, LoginActivity.class);
+        }
+        startActivity(intent);
+        finish();
     }
 }
