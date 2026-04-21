eecd0f87445be718675c62a44fbf90b1b44ae0e1
Sofia <sloukisa03@gmail.com>
2026-04-21 12:36:24 +0300

    add favorites adapter setters

--- app/src/main/java/com/example/mealapplication/ui/favourites/FavoritesAdapter.java
+++ app/src/main/java/com/example/mealapplication/ui/favourites/FavoritesAdapter.java
@@ -1,4 +1,38 @@
 package com.example.mealapplication.ui.favourites;
 
-public class FavoritesAdapter {
-}
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.mealapplication.R;
+import com.example.mealapplication.local.FavoriteMealEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FavoritesAdapter extends RecyclerView.Adapter<FavoritesAdapter.ViewHolder> {
+
+    private List<FavoriteMealEntity> favorites = new ArrayList<>();
+    private final OnFavoriteClickListener listener;
+
+    public interface OnFavoriteClickListener {
+        void onFavoriteClick(FavoriteMealEntity meal);
+        void onRemoveClick(FavoriteMealEntity meal);
+    }
+
+    public FavoritesAdapter(OnFavoriteClickListener listener) {
+        this.listener = listener;
+    }
+
+    public void setFavorites(List<FavoriteMealEntity> favorites) {
+        this.favorites = favorites;
+        notifyDataSetChanged();
+    }
+
+
