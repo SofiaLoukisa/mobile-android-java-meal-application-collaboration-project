295280b8596a99501409a130a28a2ca579955e8a
Sofia <sloukisa03@gmail.com>
2026-04-21 12:29:37 +0300

    add favorites creation

--- app/src/main/java/com/example/mealapplication/ui/favourites/FavoritesActivity.java
+++ app/src/main/java/com/example/mealapplication/ui/favourites/FavoritesActivity.java
@@ -1,4 +1,31 @@
 package com.example.mealapplication.ui.favourites;
 
-public class FavoritesActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.mealapplication.R;
+import com.example.mealapplication.adapter.MealAdapter;
+
+public class FavoritesActivity extends AppCompatActivity {
+
+    private RecyclerView favoritesRecyclerView;
+    private MealAdapter mealAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favorites);
+
+        favoritesRecyclerView = findViewById(R.id.favoritesRecyclerView);
+
+        mealAdapter = new MealAdapter(meal -> {
+
+        });
+
+        favoritesRecyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+        favoritesRecyclerView.setAdapter(mealAdapter);
+    }
 }
