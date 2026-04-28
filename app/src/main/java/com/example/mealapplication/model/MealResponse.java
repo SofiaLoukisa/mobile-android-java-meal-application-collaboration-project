3252ff9c54b7b2dc7c9307b0de564ea358699130
Sofia <sloukisa03@gmail.com>
2026-04-28 17:46:56 +0300

    bug fix

--- app/src/main/java/com/example/mealapplication/model/MealResponse.java
+++ app/src/main/java/com/example/mealapplication/model/MealResponse.java
@@ -1,11 +1,14 @@
 package com.example.mealapplication.model;
 
 import com.google.gson.annotations.SerializedName;
+
 import java.util.List;
 
 public class MealResponse {
-    @SerializedName("com/example/mealapplication/ui/meals")
+    @SerializedName("meals")
     private List<Meal> meals;
 
-    public List<Meal> getMeals() { return meals; }
+    public List<Meal> getMeals() {
+        return meals;
+    }
 }
